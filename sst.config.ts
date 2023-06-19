0697db10ac267d1822150dada1c911b19dafb1db
Nicky Marino <nicky@nickymarino.com>
2023-06-18 23:49:08 -0500

    Add auth handler

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@ import { SSTConfig } from "sst";
 import { API } from "./stacks/API";
 import { Database } from "./stacks/Database";
 import { Site } from "./stacks/Site";
+import { Authentication } from "./stacks/Authentication";
 
 export default {
   config(_input) {
@@ -13,6 +14,7 @@ export default {
   stacks(app) {
     app.stack(Database);
     app.stack(API);
+    app.stack(Authentication);
     app.stack(Site);
   },
 } satisfies SSTConfig;
