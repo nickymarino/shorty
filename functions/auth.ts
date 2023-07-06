3175b3301f897c6da65d230604a0b755961c514e
Nicky Marino <nicky@nickymarino.com>
2023-07-06 14:45:46 -0600

    Use GitHub OAuth instead of magic links

--- functions/auth.ts
+++ functions/auth.ts
@@ -1,4 +1,5 @@
-import { AuthHandler, LinkAdapter, Session } from "sst/node/auth";
+import { AuthHandler, GithubAdapter, Session } from "sst/node/auth";
+import { Config } from "sst/node/config";
 
 declare module "sst/node/auth" {
   export interface SessionTypes {
@@ -10,20 +11,38 @@ declare module "sst/node/auth" {
 
 export const handler = AuthHandler({
   providers: {
-    // Frontend
-    // location.href =
-    //  "https://api.example.com/auth/link/authorize?email=user@example.com";
-    link: LinkAdapter({
-      onLink: async (link, claims) => {
-        console.log({ link, claims });
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ link }),
-        };
-      },
-      onSuccess: async (claims) => {
-        console.log({ claims });
-        // https://example.com/?token=XXXXXX
+    /*
+      Start of flow: /auth/github/authorize
+      eg: https://j7tgyxxjg4.execute-api.us-east-1.amazonaws.com/auth/github/authorize
+
+      Callback: /auth/github/callback
+
+      >>> requests.get('https://api.github.com/user', headers={"Authorization": "Bearer ghu_XXXXXXX"})
+    */
+    github: GithubAdapter({
+      clientID: Config.GITHUB_CLIENT_ID,
+      clientSecret: Config.GITHUB_CLIENT_SECRET,
+      scope: "", // empty = read only, public values
+      onSuccess: async (tokenset, client) => {
+        console.log({ tokenset, client });
+
+        const accessToken = tokenset.access_token;
+        const response = await fetch("https://api.github.com/user", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        const body = await response.json();
+
+        const userGitHubId = body.id;
+        // TODO: from here, find our user ID based on github ID, and
+        //       return a Session.parameter with our user ID
+
+        console.log({ response });
+        console.log({ body });
+        console.log({ userGitHubId });
+
         return Session.parameter({
           redirect: "https://example.com",
           type: "user",
@@ -31,13 +50,6 @@ export const handler = AuthHandler({
             userID: "1234",
           },
         });
-      },
-      onError: async () => {
-        console.log("ERROR!");
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ message: "there was an error" }),
-        };
       },
     }),
   },
